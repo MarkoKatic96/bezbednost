fe79d43ea6e06d2c792b4ac043789626f69aa9af
CodeEatSleepRepeat <borisbibic1996@gmail.com>
2019-06-24 14:05:28 +0200

    Dodat SSL u RestTemplate

--- core-microservice/rating-service/src/main/java/com/megatravel/ratingservice/RatingServiceApplication.java
+++ core-microservice/rating-service/src/main/java/com/megatravel/ratingservice/RatingServiceApplication.java
@@ -1,20 +1,43 @@
 package com.megatravel.ratingservice;
 
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class RatingServiceApplication {
 
+	@Value("${http.client.ssl.trust-store}")
+    private Resource trustStore;
+
+	@Value("${http.client.ssl.trust-store-password}")
+    private String trustStorePassword;
+
 	@Bean
 	@LoadBalanced
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+	public RestTemplate getRestTemplate() throws Exception {
+		SSLContext sslContext = new SSLContextBuilder()
+			      .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray())
+			      .build();
+			    SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+			    HttpClient httpClient = HttpClients.custom()
+			      .setSSLSocketFactory(socketFactory)
+			      .build();
+			    HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+			    return new RestTemplate(factory);
 	}
 	
 	public static void main(String[] args) {
